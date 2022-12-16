935a655d9a5376f92d91777f95cc872cd533b930
Bracey Summers <bcsummers@gmail.com>
2022-12-16 10:28:00 -0600

    General cleanup - removed metadata file - updated pre-commit config

--- falcon_provider_storage/__init__.py
+++ falcon_provider_storage/__init__.py
@@ -1,12 +1,4 @@
 """Falcon storage module."""
 # flake8: noqa
-from .__metadata__ import (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __package_name__,
-    __url__,
-    __version__,
-)
-from .utils import LocalStorageProvider, S3StorageProvider, StorageProvider
+# first-party
+from falcon_provider_storage.utils import LocalStorageProvider, S3StorageProvider, StorageProvider
